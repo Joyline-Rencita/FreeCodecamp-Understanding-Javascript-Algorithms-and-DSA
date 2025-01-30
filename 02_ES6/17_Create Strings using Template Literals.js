28735cb08c53262eccc0efb58bc7613607dd0e16
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-01-30 23:25:03 +0530

    Update 17_Create Strings using Template Literals.js

--- 02_ES6/17_Create Strings using Template Literals.js
+++ 02_ES6/17_Create Strings using Template Literals.js
@@ -27,3 +27,29 @@ Use an iterator method (any kind of loop) to get the desired output (shown below
   '<li class="text-warning">linebreak</li>'
 ]
 
+
+
+Solution :
+
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+
+function makeList(arr) {
+  "use strict";
+
+  // Only change code below this line
+  const resultDisplayArray = [];
+  for (let item of arr) {
+    resultDisplayArray.push(`<li class="text-warning">${item}</li>`);
+  }
+  // Only change code above this line
+
+  return resultDisplayArray;
+}
+
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
