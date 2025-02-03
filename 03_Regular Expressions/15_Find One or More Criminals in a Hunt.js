63173a1bbb66779a0695febbc65f8adf62dbe676
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-03 16:09:12 +0530

    Create 15_Find One or More Criminals in a Hunt.js

---
+++ 03_Regular Expressions/15_Find One or More Criminals in a Hunt.js
@@ -0,0 +1,15 @@
+Time to pause and test your new regex writing skills. A group of criminals escaped from jail and ran away, but you don't know how many. However, you do know that they stay close together when they are around other people. You are responsible for finding all of the criminals at once.
+
+Here's an example to review how to do this:
+
+The regex /z+/ matches the letter z when it appears one or more times in a row. It would find matches in all of the following strings:
+//*******************************
+
+Solution :
+
+let crowd = 'P1P2P3P4P5P6CCCP7P8P9';
+
+let reCriminals = /C+/; // Change this line
+
+let matchedCriminals = crowd.match(reCriminals);
+console.log(matchedCriminals);
