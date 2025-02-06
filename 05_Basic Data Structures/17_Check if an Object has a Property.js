d0d6bc5234b2022ec8b4441dab5b28815f3c57ce
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-06 11:38:03 +0530

    Update 17_Check if an Object has a Property.js

--- 05_Basic Data Structures/17_Check if an Object has a Property.js
+++ 05_Basic Data Structures/17_Check if an Object has a Property.js
@@ -29,15 +29,34 @@ let users = {
   }
 };
 
-function isEveryoneHere(obj) {
-  // Only change code below this line
-  return 'Alan' in users &&
-  'Jeff' in users && 
-  'Sarah' in users && 
-  'Ryan' in users
-  // Only change code above this line
-}
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
 
+function isEveryoneHere(obj) {
+  // change code below this line
+  if (obj.hasOwnProperty('Alan') && obj.hasOwnProperty('Jeff') && obj.hasOwnProperty('Sarah') && obj.hasOwnProperty('Ryan')){
+    return true;
+  } else {
+    return false;
+  }
+  // change code above this line
+}
 console.log(isEveryoneHere(users));
 
 >> True
