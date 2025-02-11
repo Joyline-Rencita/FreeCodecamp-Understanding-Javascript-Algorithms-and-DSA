216e7a37182d6acb6db21806bf832095ae05f0e5
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-11 17:35:19 +0530

    Update 15_Remember to Set the Constructor Property when Changing the Prototype.js

--- 07_Object Oriented Programming/15_Remember to Set the Constructor Property when Changing the Prototype.js
+++ 07_Object Oriented Programming/15_Remember to Set the Constructor Property when Changing the Prototype.js
@@ -18,3 +18,25 @@ Bird.prototype = {
   }
 };
 Define the constructor property on the Dog prototype.
+
+
+  //**********************************
+
+
+  Solution :
+
+function Dog(name) {
+  this.name = name;
+}
+
+// Only change code below this line
+Dog.prototype = {
+  constructor : Dog,
+  numLegs: 4,
+  eat: function() {
+    console.log("nom nom nom");
+  },
+  describe: function() {
+    console.log("My name is " + this.name);
+  }
+};
