a23065f132c4e98e5d09f0c467b071b6b2dd7887
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-12 23:01:25 +0530

    Update 08_Implement map on a Prototype.js

--- 08_Functional Programming/08_Implement map on a Prototype.js
+++ 08_Functional Programming/08_Implement map on a Prototype.js
@@ -13,10 +13,12 @@ Write your own Array.prototype.myMap(), which should behave exactly like Array.p
 
 Array.prototype.myMap = function(callback) {
   const newArray = [];
-  // Only change code below this line
   for (let i = 0; i < this.length; i++) {
-    newArray.push(callback(this[i]));
+    newArray.push(callback(this[i], i, this));
   }
-  // Only change code above this line
   return newArray;
 };
+
+// Test case
+const s = [23, 65, 98, 5];
+const doubled_s = s.myMap(item => item * 2);
