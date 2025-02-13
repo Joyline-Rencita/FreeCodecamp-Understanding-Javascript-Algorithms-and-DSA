b7eba2ccbcd824a78e3c2331276918444e2b7c72
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2025-02-13 14:08:53 +0530

    Update 02_Diff Two Arrays.js

--- 09_Intermediate Algorithm Scripting/02_Diff Two Arrays.js
+++ 09_Intermediate Algorithm Scripting/02_Diff Two Arrays.js
@@ -6,3 +6,10 @@ Note: You can return the array with its elements in any order.
 
   Solution :
 
+function diffArray(arr1, arr2) {
+  return arr1.concat(arr2).filter(value => !arr1.includes(value) || !arr2.includes(value))
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
+
+>> [ 4 ]
